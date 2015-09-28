fb7140ed11197fa4c555792bbe49c37b78f09d90
tongtonk <davidhoverson@yahoo.com>
2015-09-28 09:24:12 -0500

    Adds console log

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ var students = require('./public/assets/data/carousel');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/' , function(req, res){
+        console.log('hit home end point');
         res.sendFile(__dirname + '/public/views/index.html');
 });
 
@@ -15,7 +16,7 @@ app.get('/carousel' , function(req, res){
     res.send(students);
 });
 
-var server = app.listen(process.env.port, function(){
+var server = app.listen(process.env.PORT, function(){
     var port = server.address().port;
     console.log("Listening on port: ", port);
 });
